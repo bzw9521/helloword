224cb99567b366a7603962306c342793cc9e563e
bzw9521 <bzw9521@outlook.github.com>
2019-03-05 11:57:09 +0800

    提交water

---
+++ src/main/java/com/example/watermanager/WatermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.watermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermanagerApplication.class, args);
+    }
+
+}
